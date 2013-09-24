c0a184847b16983fe369d2f1462831deb9d6dd02
dzamie <dzami3@gmail.com>
2013-09-24 10:18:23 -0400

    added ship sizes

--- src/com/wildelake/battleship/Ship.java
+++ src/com/wildelake/battleship/Ship.java
@@ -4,6 +4,14 @@ public class Ship {
 	private Ship() {
 		
 	}
+	
+	/* :: Sizes ::
+	 * AC: 5 tiles
+	 * BS: 4 tiles
+	 * Sub: 3 tiles
+	 * Cruiser: 3 tiles
+	 * Destroyer: 2 tiles
+	 */
 
 	public class AircraftCarrier extends Ship {
 
